429da4990f0956e3100b4e1e4e1e8d609472d85f
francescapetra <francescalaurapetra@gmail.com>
2021-04-08 15:05:46 +0200

    test su cards

--- script/script.js
+++ script/script.js
@@ -2,8 +2,15 @@
   {
     el:"#root",
     data:{
-      message: "",
-      miaClasse : "red",
+      albums: [
+        {
+          poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+          title: "New Jersey",
+          author: "Bon Jovi",
+          genre: "Rock",
+          year: "1988"
+        }
+      ],
     },
     methods:{
     },
